6e737ed4c2ca40d144c693f2e83501f5c1ec1173
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-13 12:22:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(picupLocation){
+    const hqLocation = 42
+    return picupLocation-hqLocation
+}
+console.log(distanceFromHqInBlocks(50))
+
+function distanceFromHqInFeet(){
+    const block = 264
+    return distanceFromHqInBlocks(50)*block
+}
+console.log(distanceFromHqInFeet())
+
+function distanceTravelledInFeet(a,b){
+    return (b-a)*264
+}
+console.log(43,48)
+   
+
+
+
+
